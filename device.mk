e0aef327723a2035a062d0c45580cfeb90e73ed8
RonGokhale <cips173@gmail.com>
2013-11-01 09:56:29 +0530

    e610: aosp bringup modified by bart452 Change-Id: Id4a815cbd2f97565bb32986222315c14c8d65424
    Conflicts:
    	AndroidProducts.mk
    	device.mk
    	full_e610.mk
    	vendorsetup.sh

--- device.mk
+++ device.mk
@@ -1,5 +1,18 @@
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles.xml:system/etc/media_profiles.xml
+#
+# Copyright (C) 2011 The Android Open-Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
 $(call inherit-product, device/lge/msm7x27a-common/msm7x27a-common.mk)
 $(call inherit-product-if-exists, vendor/lge/p700/p700-vendor.mk)
@@ -54,6 +67,7 @@ PRODUCT_PACKAGES += \
     Camera
 
 
-PRODUCT_NAME := full_p700
-PRODUCT_DEVICE := p700
-PRODUCT_MODEL := LG-P700
+# Enable for debugging
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.debuggable=1 \
+    persist.service.adb.enable=1
